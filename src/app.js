e50121847b7e7492379321a7c26ad2ee7f3f09a9
irinag00 <irinagorlino@gmail.com>
2024-01-24 23:03:02 -0300

    finalizado el carrito

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { ProductManager } from "./ProductManager.js";
+import { CartManager } from "./CartManager.js";
 import routerProducts from "./routers/products.router.js";
+import routerCart from "./routers/cart.router.js";
 
 const app = express();
 const PORT = 8080;
 
 const pathProducts = "./src/data/products.json";
+const pathCart = "./src/data/cart.json";
 export const productManager = new ProductManager(pathProducts);
+export const cartManager = new CartManager(pathCart);
 
 //middlewares
 app.use(express.json());
@@ -14,5 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use("/api/products", routerProducts);
+app.use("/api/carts", routerCart);
 
 app.listen(PORT, () => console.log("Servidor con express en puesto: ", PORT));
